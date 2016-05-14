bb7d0544292f3891e5ff179ccbc0a3158c40ca41
meomeo038 <xuansontx@gmail.com>
2016-05-14 13:05:05 +0900

    Add files via upload

---
+++ LambdaByeffectivelyFinal.java
@@ -0,0 +1,26 @@
+interface Iamfunctioninterface { String move(); }
+
+public class LambdaByeffectivelyFinal{
+
+    String instanceVarians = "i am an instance varians ";
+    
+    void everyonePlay(boolean localParameter) {
+        String localVarial = "i am is local varial"; 
+        //approach = "uncomment this line to create error effectively final ";
+        play(() -> instanceVarians);
+        play(() -> localParameter ? "localParameter is true and is paramater function": "localParameter is false and is paramater function"); 
+        play(() -> localVarial);
+    }
+    
+    void play(Iamfunctioninterface g) { 
+        System.out.println(g.move());
+    }
+    
+    public static void main(String []args){
+        new LambdaByeffectivelyFinal().everyonePlay(true);
+    }
+    
+    
+
+}
+
